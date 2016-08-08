d21a182fcc3449ed75ec3c5290a9a46c3268b8f7
Mitch Allen <mitch.c.allen@gmail.com>
2016-08-08 12:47:47 -0700

    ran jslint against index.js

--- index.js
+++ index.js
@@ -8,17 +8,17 @@
 "use strict";
 
 module.exports = {
-    
-    notNull: function(obj, eMsg) {
-        if( !obj ) {
+
+    notNull: function (obj, eMsg) {
+        if (!obj) {
             let err = new Error(eMsg);
             console.error(err.message);
             throw err;
         }
     },
 
-    notError: function(err) {
-        if( err ) {
+    notError: function (err) {
+        if (err) {
             console.error(err.message);
             throw err;
         }
